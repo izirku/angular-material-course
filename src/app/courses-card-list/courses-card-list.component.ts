87f1b94a59edf558172f497a1a764232a63aa11f
izirku <izirku@users.noreply.github.com>
2020-03-23 15:53:46 -0500

    finished section

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,27 +1,33 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {CourseDialogComponent} from "../course-dialog/course-dialog.component";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Course } from '../model/course';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  constructor(private dialog: MatDialog) {}
 
-    constructor() {
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-
-    }
-
-    editCourse({description, longDescription, category}:Course) {
-
-    }
-
+  editCourse({ description, longDescription, category }: Course) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true; // auto focus first input
+    dialogConfig.data = {
+      description,
+      longDescription,
+      category
+    };
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+    dialogRef
+      .afterClosed()
+      .subscribe(val => console.log('dialog returned with:', val));
+  }
 }
